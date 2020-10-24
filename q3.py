3c15a988b6a27a0c0ac7981711a4abb558e49c1d
JordandEntremont <73398362+JordandEntremont@users.noreply.github.com>
2020-10-24 14:09:51 -0400

    Add files via upload

--- q3.py
+++ q3.py
@@ -1,5 +1,13 @@
 def main(int1, int2):
-   return "Your return here"
+    if int1 > 0 and int2 > 0:
+        while int1 % 2 == 0 and int1 % 2 == 0:
+            int1 /= 2
+            int2 /= 2
+            int1 /= 3
+            int2 /= 3
+            print("The reduced fraction is: {}/{}".format(int1 , int2))
+
+ #  return int1 , int2
 
 if __name__ == "__main__":
     print(
